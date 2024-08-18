25e460e0073bfca20d3fb1a932133447e8ba825e
vijay767492 <vijaydandu59@gmail.com>
2024-08-18 16:23:34 +0530

    loops

---
+++ practoloop.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class practoloop {
+    public static void main(String[] args) {
+        Scanner ss=new Scanner(System.in);
+        //loops and (break and continue)
+        // System.out.print("enter the number :");
+        // int i = 0;
+        // do {
+        //     i++;
+        //     if (i == 10) {
+        //         System.out.println("it is skipped due to continue");
+                 
+        //         continue;
+        //     }
+        //     System.out.println(i);
+            
+        // } while (i <= 20);
+        
+       
+// for ( int i=3;i<=20;i++){
+//     if (i==10){
+//         System.out.println("its skipped:");
+//         continue;
+//     }
+//     System.out.println(i);
+
+// int i=0;
+// while (i<=20){i++;
+//     if (i==10){System.out.println("its skipped :");  continue;}
+//     System.out.println(i);
+
+// }
+
+int i=0;
+do {
+    i++;
+    if(i==10){
+        System.out.println("skipped :");
+        continue;}
+System.out.println(i); }while(i<20);
+
+
+
+}}
+
